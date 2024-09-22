8cf5c76dacd51604450725fb11b8f67fc82fe1d2
VALDEZ R. <renzv753@gmail.com>
2024-09-22 15:34:04 +0800

    Create LA2_VALDEZ R_CS2C.js

---
+++ LA2_VALDEZ R_CS2C.js
@@ -0,0 +1,10 @@
+var name = "[Renz Galcon Valdez]";
+var birthDate = "[April 13, 2005]";
+var birthPlace = "[Resonable Hospital, Candon City, Ilocos Sur, Philippines 2710]";
+var address = "[Oaig Daya, Candon City, Ilocos Sur, Philippines 2710]";
+var course = "[Bachelor Of Science in Computer Science]";
+var year = "2";
+var dreamJob = "[Game Developer]";
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace.toUpperCase() + ", and currently living at " + address.toUpperCase()+".");
+console.log(name.toLowerCase() + " is taking up " + course.toLowerCase() + " and dreams to be " + dreamJob + " after graduation.")
